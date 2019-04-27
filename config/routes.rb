b8dd5aa78e89c79bd18492e04a9ff3a9cfa4d91a
brennanmrose <brennan.m.rose@gmail.com>
2019-04-27 11:25:52 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
+  root 'sessions#home' 
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
