baabc6030fc15aee16d3c73ad6d6489244849d2a
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 13:45:06 -0400

    add validations on db

--- db/migrate/20190427015259_create_users.rb
+++ db/migrate/20190427015259_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
+      t.string :first_name, allow_nil: false
+      t.string :last_name, allow_nil: false
+      t.string :email, unique: true, allow_nil: false
       t.string :password_digest
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_173620) do
+ActiveRecord::Schema.define(version: 2019_04_28_173847) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reported_issues", force: :cascade do |t|
     t.text "chief_complaint"
