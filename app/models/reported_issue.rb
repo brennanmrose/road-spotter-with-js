bc77a7c05387fd5e312f60e1f0a7ff92cd72b013
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 14:38:17 -0400

    add validations to reported_issues

--- app/models/reported_issue.rb
+++ app/models/reported_issue.rb
@@ -1,4 +1,5 @@
 class ReportedIssue < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
+	validates :name, uniqueness: true, presence: true
 end
--- db/migrate/20190428173847_create_categories.rb
+++ db/migrate/20190428173847_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
-      t.string :name, allow_nil: false
+      t.string :name, unique: true, allow_nil: false
 
       t.timestamps
     end
