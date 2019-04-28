58d365eb84590ef2c41c40f72c16d44b73f5f489
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 13:46:21 -0400

    add validations to db

--- db/migrate/20190428173620_create_reported_issues.rb
+++ db/migrate/20190428173620_create_reported_issues.rb
@@ -1,14 +1,14 @@
 class CreateReportedIssues < ActiveRecord::Migration[5.2]
   def change
     create_table :reported_issues do |t|
-      t.text :chief_complaint
-      t.string :transportation_type
-      t.string :street_address
-      t.string :locality
-      t.string :region
-      t.integer :postal_code
-      t.integer :user_id
-      t.integer :category_id
+      t.text :chief_complaint, allow_nil: false
+      t.string :transportation_type, allow_nil: false
+      t.string :street_address, allow_nil: false
+      t.string :locality, allow_nil: false
+      t.string :region, allow_nil: false
+      t.integer :postal_code, allow_nil: false
+      t.integer :user_id, allow_nil: false
+      t.integer :category_id, allow_nil: false
 
       t.timestamps
     end
