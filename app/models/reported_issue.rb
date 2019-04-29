bd3bb0833dbb158e2e617dcd6f6a5ba96215dcfa
brennanmrose <brennan.m.rose@gmail.com>
2019-04-29 08:32:35 -0400

    add validation on postal code

--- app/models/reported_issue.rb
+++ app/models/reported_issue.rb
@@ -2,4 +2,5 @@ class ReportedIssue < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
 	validates :chief_complaint, :transportation_type, :street_address, :locality, :region, :postal_code, :user_id, :category_id, presence: true
+	validates :postal_code, length: { is: 5 }
 end
