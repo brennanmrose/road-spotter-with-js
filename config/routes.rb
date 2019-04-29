c955ad0b266036a7381aaf3b1ae9d86b6baf612d
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 21:03:36 -0400

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :categories do
   	resources :reported_issues, only: [:show, :index]
+  end
 
   resources :reported_issues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
