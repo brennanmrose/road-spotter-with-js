0ee008ec5ae1172acc86ecbbe90daf33691f7a51
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 21:20:38 -0400

    fix styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
 
 	private
 
-		def current_user
-			@current_user ||= User.find(session[:user_id])
-		end
+	def current_user
+		@current_user ||= User.find(session[:user_id])
+	end
 
 end
