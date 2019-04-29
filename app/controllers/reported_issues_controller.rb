b043d896512c2deb160ae3c2e49a4cc937a0bf26
brennanmrose <brennan.m.rose@gmail.com>
2019-04-29 08:28:14 -0400

    create strong params

--- app/controllers/reported_issues_controller.rb
+++ app/controllers/reported_issues_controller.rb
@@ -22,4 +22,10 @@ class ReportedIssuesController < ApplicationController
 	def destroy
 	end
 
+	private
+
+	def reported_issue_params
+		params.require(:reported_issue).permit(:chief_complaint, :transportation_type, :street_address, :locality, :region, :postal_code, :user_id, :category_id)
+	end
+
 end
