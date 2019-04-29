6c29d94d8802e3cefa35fe5e48f7c120dd88e4c8
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 22:02:16 -0400

    build create method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,11 @@ class CategoriesController < ApplicationController
 	end
 
 	def create
+		@category = Category.find_by(name: params[:category][:name])
+		if @category.nil?
+			@category = Category.create(category_params)
+		end
+		redirect_to category_path(@category)
 	end
 
 	def show
