5fdd28f032aa2fa903ec2049253d5cfb9b9e12f1
brennanmrose <brennan.m.rose@gmail.com>
2019-04-28 21:27:09 -0400

    update authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,10 @@ class SessionsController < ApplicationController
 
 	def create
 		@user = User.find_by(email: params[:user][:email])
-		if @user && @user.authenticate(password: params[:user][:password])
-			session[:user_id] = @user.id 
-			redirect_to user_path(@user)
-		else
-			redirect_to :login
-		end
+		@user = @user.try(:authenticate, params[:user][:password])
+		return redirect_to login_path unless @user
+		session[:user_id] = @user.id 
+		redirect_to user_path(@user)
 	end 
 
 	def home
