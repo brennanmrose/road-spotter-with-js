ff0f05ffb55afa3d7f63d4d7a2441f0976ed7124
brennanmrose <brennan.m.rose@gmail.com>
2019-04-29 21:30:49 -0400

    add category name method

--- app/models/reported_issue.rb
+++ app/models/reported_issue.rb
@@ -3,4 +3,14 @@ class ReportedIssue < ApplicationRecord
 	belongs_to :category
 	validates :chief_complaint, :transportation_type, :street_address, :locality, :region, :postal_code, :user_id, :category_id, presence: true
 	validates :postal_code, length: { is: 5 }
+
+	def category_name
+		self.try(:category).try(:name)
+	end
+
+	def category_name=(name)
+		category = Category.find_or_create_by(name: name)
+		self.category = category
+	end
+	
 end
