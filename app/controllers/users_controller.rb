198620a9de2b79964160888017a9970b1c99b05d
brennanmrose <brennan.m.rose@gmail.com>
2019-05-01 21:38:18 -0400

    redirect to cats path instead of user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,14 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id 
-			redirect_to user_path(@user)
+			redirect_to categories_path
 		else
 			render :new
 		end
+	end
+
+	def show
+		redirect_to categories_path
 	end
 	
 	private
