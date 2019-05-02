8663b4e7550b3565974d442df51987c0b247d990
brennanmrose <brennan.m.rose@gmail.com>
2019-05-01 21:43:40 -0400

    remove comments

--- app/helpers/reported_issues_helper.rb
+++ app/helpers/reported_issues_helper.rb
@@ -1,11 +1,5 @@
 module ReportedIssuesHelper
 
-	# def transportation_types
-	# 	ReportedIssue.all.collect do |issue|
-	# 		@transportation_types = issue.transportation_type
-	# 	end
-	# end
-
 	def if_current_user_display_edit
 		@current_user ||= User.find(session[:user_id])
 		if @current_user.id == @reported_issue.user_id
