f21bb12dc29c03878a8513c7e2647e790b987278
brennanmrose <brennan.m.rose@gmail.com>
2019-05-01 21:25:22 -0400

    add conditional if current user display edit link

--- app/helpers/reported_issues_helper.rb
+++ app/helpers/reported_issues_helper.rb
@@ -6,4 +6,11 @@ module ReportedIssuesHelper
 	# 	end
 	# end
 
+	def if_current_user_display_edit
+		@current_user ||= User.find(session[:user_id])
+		if @current_user.id == @reported_issue.user_id
+			link_to 'Edit Issue', edit_reported_issue_path(@reported_issue)
+		end
+	end
+
 end
