e800453c71c8ee834c94383dfd9cfdd7368f53a2
brennanmrose <brennan.m.rose@gmail.com>
2019-05-02 07:25:05 -0400

    if current user display delete

--- app/helpers/reported_issues_helper.rb
+++ app/helpers/reported_issues_helper.rb
@@ -1,10 +1,20 @@
 module ReportedIssuesHelper
 
 	def if_current_user_display_edit
-		@current_user ||= User.find(session[:user_id])
-		if @current_user.id == @reported_issue.user_id
+		if current_user?
 			link_to 'Edit Issue', edit_reported_issue_path(@reported_issue)
 		end
+	end
+
+	def if_current_user_display_delete
+		if current_user?
+			link_to 'Delete Issue', reported_issue_path(@reported_issue), method: 'delete'
+		end
+	end
+
+	def current_user?
+		@current_user ||= User.find(session[:user_id])
+		@current_user.id == @reported_issue.user_id
 	end
 
 end
