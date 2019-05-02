1b389587bb721602503ffdd7c17a7888805fe551
brennanmrose <brennan.m.rose@gmail.com>
2019-05-02 07:49:22 -0400

    add require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
+	before_action :current_user
 
 	private
 
 	def current_user
 		@current_user ||= User.find(session[:user_id])
+	end
+
+	def logged_in?
+		current_user.id.present?
+	end
+
+	def require_login
+		unless logged_in?
+			redirect_to login_path
+		end
 	end
 
 end
