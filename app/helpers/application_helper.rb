0f2fc05d1bcda89e678eae9e665ea236b921e904
brennanmrose <brennan.m.rose@gmail.com>
2019-05-03 21:38:30 -0400

    add link to user show if user present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,18 @@ module ApplicationHelper
 		end
 	end
 
+	def if_logged_in_display_user_show
+		if user_present?
+			link_to 'User Account', user_path(@user)
+		end
+	end
+
+	private
+
+	def user_present?
+		if session[:user_id].present?
+			@user = User.find(session[:user_id])
+		end
+	end
+
 end
