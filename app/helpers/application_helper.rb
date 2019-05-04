6d868ba045353826d1c05ec7eb8a3612bf6c789d
brennanmrose <brennan.m.rose@gmail.com>
2019-05-03 21:41:54 -0400

    add link to reported issues if logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,12 @@ module ApplicationHelper
 		end
 	end
 
+	def if_logged_in_display_reported_issues
+		if user_present?
+			link_to 'All Reported Issues', reported_issues_path
+		end
+	end
+
 	private
 
 	def user_present?
