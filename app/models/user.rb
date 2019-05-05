2e9953560f289bbe46efe0fce26aaff3bc194574
brennanmrose <brennan.m.rose@gmail.com>
2019-05-05 16:57:44 -0400

    remove validation on last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :reported_issues
 	has_many :categories, through: :reported_issues
-	validates :first_name, :last_name, :email, presence: true
+	validates :first_name, :email, presence: true
 	validates :email, uniqueness: true
 end
--- db/migrate/20190427015259_create_users.rb
+++ db/migrate/20190427015259_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :first_name, allow_nil: false
-      t.string :last_name, allow_nil: false
+      t.string :last_name
       t.string :email, unique: true, allow_nil: false
       t.string :password_digest
 
