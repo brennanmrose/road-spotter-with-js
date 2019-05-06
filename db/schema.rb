43cd1591b9076f45773312a8b6ceea051f8440de
brennanmrose <brennan.m.rose@gmail.com>
2019-05-06 09:01:11 -0400

    add column for uid omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_173847) do
+ActiveRecord::Schema.define(version: 2019_05_06_125803) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_173847) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end
