29f1b393035a5d6b9856e309f39e8165c9c490ab
brennanmrose <brennan.m.rose@gmail.com>
2019-05-06 08:21:37 -0400

    add route for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback', to: 'sessions#create'
 
   resources :users, only: [:index, :show, :create]
 
