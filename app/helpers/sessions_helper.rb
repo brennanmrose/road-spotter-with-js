ce0a4a23916ab2b2820873a82b7697b6d5d34ac0
brennanmrose <brennan.m.rose@gmail.com>
2019-05-08 08:52:12 -0400

    only display fb login if logged out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,27 @@
 module SessionsHelper
 
 	def if_logged_out_display_signup
-		if session[:user_id].nil?
+		if user_nil?
 			link_to 'Sign Up', signup_path
 		end
 	end
 
 	def if_logged_out_display_login
-		if session[:user_id].nil?
+		if user_nil?
 			link_to 'Log In', login_path
 		end
+	end
+
+	def if_logged_out_display_fb_signup
+		if user_nil?
+			link_to 'Sign in through Facebook', '/auth/facebook'
+		end
+	end
+
+	private
+
+	def user_nil?
+		session[:user_id].nil?
 	end
 
 end
