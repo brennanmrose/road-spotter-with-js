d76481894995ef264f6658d6ed7bfa5c27f1e2cc
brennanmrose <brennan.m.rose@gmail.com>
2019-05-08 08:44:04 -0400

    handle ri new for nested and non-nested routes

--- app/helpers/reported_issues_helper.rb
+++ app/helpers/reported_issues_helper.rb
@@ -12,6 +12,14 @@ module ReportedIssuesHelper
 		end
 	end
 
+	def link_to_ri_new_nested_or_non_nested
+		if @category.present?
+			link_to 'Report a New Issue', new_category_reported_issue_path(@category)
+		else
+			link_to 'Report a New Issue', new_reported_issue_path
+		end
+	end
+
 	def current_user?
 		@current_user ||= User.find(session[:user_id])
 		@current_user.id == @reported_issue.user_id
