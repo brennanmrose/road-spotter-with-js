f6dbee3047d274fb1df78b9f18b0aa23ff34d643
brennanmrose <brennan.m.rose@gmail.com>
2019-05-15 07:36:53 -0400

    remove category name key

--- app/models/reported_issue.rb
+++ app/models/reported_issue.rb
@@ -7,12 +7,12 @@ class ReportedIssue < ApplicationRecord
 
 	accepts_nested_attributes_for :category
 
-	def category_name
-		category.try(:name)
-	end
+	# def category_name
+	# 	category.try(:name)
+	# end
 
-	def category_name=(name)
-		self.category = Category.find_or_initialize_by(name: name)
-	end
+	# def category_name=(name)
+	# 	self.category = Category.find_or_initialize_by(name: name)
+	# end
 	
 end
