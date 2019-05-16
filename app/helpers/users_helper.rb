db36a6acd9b6899cf1914e67359c0e32780c74e8
brennanmrose <brennan.m.rose@gmail.com>
2019-05-16 07:30:49 -0400

    update name of helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 
-	def if_no_reported_issues_present
+	def if_no_reported_issues_present_display_message
 		if @user.reported_issues.empty?
 			'You have not yet created a reported issue'
 		end
