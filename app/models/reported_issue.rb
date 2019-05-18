844e3e06d3f5e8ae657c9cbdb012c826900793ef
brennanmrose <brennan.m.rose@gmail.com>
2019-05-17 20:23:10 -0400

    update validations

--- app/models/reported_issue.rb
+++ app/models/reported_issue.rb
@@ -1,12 +1,12 @@
 class ReportedIssue < ApplicationRecord
-	validates :chief_complaint, :transportation_type, :street_address, :locality, :region, :postal_code, :user_id, :category_id, presence: true
+	validates :chief_complaint, :transportation_type, :street_address, :locality, :region, :postal_code, :user, :category, presence: true
 	validates :postal_code, length: { is: 5 }
 
 	belongs_to :user
 	belongs_to :category
 
 	scope :find_by_postal_code, -> (postal_code){where("postal_code = ?", postal_code)}
-	
+
 	def category_attributes=(attributes)
 		category = Category.find_or_initialize_by(name: name)
 		self.category = category if category.valid?
