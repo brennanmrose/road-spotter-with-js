fa51855637e406c6d1ea15801c0d8dafc0f8807c
brennanmrose <brennan.m.rose@gmail.com>
2019-06-26 09:11:43 -0400

    add document.ready

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,4 +1,10 @@
-$(function () {
-	console.log('category.js is loaded...');
+// $(function () {
+	// console.log('category.js is loaded...');
 	// listenForClick();
+// });
+
+$(document).ready(function() {
+
+    console.log('categories.js is loaded')
+
 });
