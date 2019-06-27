2824fe2bb7a408fa4edc0f843625623b6db74f5b
brennanmrose <brennan.m.rose@gmail.com>
2019-06-27 07:40:25 -0400

    display show

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -20,11 +20,14 @@ const bindClickHandlers = () => {
 
 	$(document).on('click', ".show_link", function(e) {
 		e.preventDefault();
+		$('#app-container').html('')
 		let id = $(this).attr('data-id')
 		fetch(`/categories/${id}.json`)
 		.then(response => response.json())
 		.then(category => {
-			console.log(category)
+			let newCategory = new Category(category)
+			let categoryHtml = newCategory.formatShow()
+			$('#app-container').append(categoryHtml)
 		})
 	})
 }
@@ -38,6 +41,13 @@ function Category(category) {
 Category.prototype.formatIndex = function() {
 	let categoryHtml = `
 		<a href="/categories/${this.id}" data-id="${this.id}" class="show_link"<h1>${this.name}</h1></a>
+	`
+	return categoryHtml
+}
+
+Category.prototype.formatShow = function() {
+	let categoryHtml = `
+		<h3>${this.name}</h3>
 	`
 	return categoryHtml
 }
